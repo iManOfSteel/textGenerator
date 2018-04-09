4d3b6e937c1435a9541adb3f5110efae60b400a9
let4ik <kozhevnikov.sv@phystech.edu>
2018-04-09 15:18:07 +0300

    some small changes

--- generate.py
+++ generate.py
@@ -14,14 +14,14 @@ def main():
     with open(args.model, 'rb') as f:
         model = pickle.load(f)
 
-    max_output_len = 1000
+    max_output_len = 100
     window = None
     if args.seed is not None:
         window = tuple(args.seed.split(' '))
     for i in range(0, args.length, max_output_len):
         text = model.make_sentence(beginning=window,
                                    length=min(max_output_len, args.length - i))
-        print(text, end=' ')
+        print(text)
         window = tuple(text.split()[len(text) - model.order:])
     if args.output is not None:
         output_file.close()
